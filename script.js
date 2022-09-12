513233b39f7d2b913f2468648e95cc37519e926e
Hodoronk <tanasa.andre@hotmail.com>
2022-09-12 11:32:48 +0300

    issue with rows duplicating

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 const container = document.querySelector('.container');
-const input = document.querySelector('.input');
 const res = document.querySelector('#result');
 
 
-input.oninput = function(){
-    res.innerHTML = this.value;
 
-}
+const input = document.querySelector('.input');
+let iVal;
+input.addEventListener ('click', function(e){
+    iVal = input.value;
+    console.log(`ival is ${iVal}`);
+    let num = iVal * iVal;
+    console.log(`num is ${num}`);
 
-let iVal = input.value;
 
 
-let num = iVal * iVal;
-for(i = 1; i <= num; i++){
+for(let i = 1; i <= num; i++){
     const newDiv = document.createElement('div');
     newDiv.id = 'r' + i;
     newDiv.classList = 'gridbox';
     container.appendChild(newDiv);
+    console.log(i);
 
 }
 
+let pix = 500 / iVal;
+let sidePix = pix / 4;
+container.style.cssText = `grid-template-columns: repeat(${iVal}, ${pix}px);`;
+// container.style.cssText = `grid-template-rows: repeat${iVal}, ${pix}px`;
+
+//rows compound each time aparently;
+console.log(`ival at the very end is ${iVal}`);
+console.log(`num at the end is ${num}`);
 
 
-let rowNum = iVal;
-let pix = 500 / rowNum;
-container.style.cssText = `grid-template-columns: repeat(${rowNum}, ${pix}px);`;
+});
 
-console.log(rowNum);
+console.log(iVal);

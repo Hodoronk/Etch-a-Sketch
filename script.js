bf8c2decfab59d82329090a56290f7328abaa8f1
Hodoronk <tanasa.andre@hotmail.com>
2022-09-14 15:47:31 +0300

    Rainbow mode added

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
 const container = document.querySelector('.container');
 const res = document.querySelector('#myValue');
 const input = document.querySelector('.input');
-const rainbow = document.getElementsByClassName('.rainbow')
+const rainbow = document.querySelector('#rainbow')
 const colorPick = document.querySelector('#cPick');
 const clearButton = document.querySelector('#resButton');
 let rainbowCheck = 0;
 let iVal;
+let a, b, c = 0;
+
+
+rainbow.addEventListener('click', function(e){
+    console.log(rainbowCheck);
+    if(rainbowCheck === 0){
+        rainbowCheck = 1;
+    }else if(rainbowCheck === 1){
+        rainbowCheck =0
+    }
+})
+
 
 function doThing(){
     res.innerHTML = `${input.value} x ${input.value}`
@@ -25,8 +37,14 @@ for(let i = 1; i <= num; i++){
     newDiv.classList = 'gridbox';
     container.appendChild(newDiv);
     console.log(i);
-    newDiv.addEventListener('click', function(){
-        newDiv.classList.toggle('rStyle');
+    newDiv.addEventListener('click', function(e){
+        console.log(rainbowCheck);
+
+        if(rainbowCheck === 0){
+        newDiv.style.cssText = 'background-color:black';}
+        else if(rainbowCheck === 1){    
+            newDiv.style.cssText = `background-color:${'#' + Math.floor(Math.random() * 16777215).toString(16)}`;
+        }
         });
 
 }
@@ -42,7 +60,7 @@ function removeAllChildNodes(parent){
 
 
 
-// I can't manage to make input.addEventListener take doThing, so instead it takes the whole thing again. Need a fix for this
+// I can't manage to make input.addEventListener take doThing as a function, so instead I made it take the whole thing again. Need a fix for this
 doThing();
 input.addEventListener ('click', function(e){
     res.innerHTML = `${input.value} x ${input.value}`
@@ -74,4 +92,3 @@ clearButton.addEventListener('click', function(e){
     doThing();
 });
 
-// rainbow.addEventListener('click')
